17cd209e6f43134a3d160ba72b02c59a02120ede
yetingting <410418566@qq.com>
2017-10-25 18:22:14 +0800

    更改路由版本

--- app/index.js
+++ app/index.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Router, hashHistory, HashRouter, Route } from 'react-router-dom'
-import Home from './pages/home/index'
-import { defaultCarousel } from './pages/carousel'
+import { Router, hashHistory } from 'react-router'
 import 'antd/dist/antd.css'
 import "./style/index.less"
+// 路由集合
+import routers from './routers'
 
 ReactDOM.render(
 	(
-		<HashRouter history={hashHistory}>
-			<div className="router-list">
-				<Route exact path="/" component={Home} />
-				{/*轮播图*/}
-				<Route path="/carousel/default" component={defaultCarousel} />
-			</div>
-		</HashRouter>
+		<Router history={hashHistory}>
+			{routers}
+		</Router>
 	),
 	document.getElementById('example')
 );
